3816add0208b825c043fc0b444676e55cbb44a50
rm17moses <mosesmoletsane17@gmail.com>
2023-10-09 15:48:54 +0200

    Initialisation Error

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from create_table import db
 from config import Config
 from rf_config import rf_Config
 from ann_folder.app_ann import app_ann
-from rf_folder.app_rf import app_rf, init_app
-import sqlite3
-
+from rf_folder.app_rf import app_rf
 
 app = Flask(__name__, template_folder="templates")
 app.config['UPLOAD_FOLDER'] = 'uploads'  # Set UPLOAD_FOLDER for the Flask application
+app.secret_key = 'contamination'  # Set the secret key
 
-# Register blueprints and other configurations after setting UPLOAD_FOLDER
-app.config.from_object(Config)
-app.app_context().push()
-db.init_app(app)
-
-def init_app(app):
-    UPLOAD_FOLDER = 'uploads'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.secret_key = 'contamination'
-
-
+# Register blueprints
 app.register_blueprint(app_ann, url_prefix="/ann")
 app.register_blueprint(app_rf, url_prefix="/rf")
+
+# Load the configuration
+app.config.from_object(Config)
+app.config.from_pyfile('config.py')
+
+# Initialize the database
+db.init_app(app)
+
+# Create the tables
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
@@ -37,16 +37,5 @@ def home():
     else:
         return render_template("homepage.html")
 
-# Load the configuration
-app.config.from_pyfile('config.py')
-
-# Initialize the database
-db.init_app(app)
-
-# Create the tables
-with app.app_context():
-    db.create_all()
-
 if __name__ == '__main__':
-    init_app(app)
     app.run(debug=True)
